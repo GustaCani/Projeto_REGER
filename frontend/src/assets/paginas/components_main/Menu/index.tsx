77a2c086cdf84419ef636ad4343a690f1b519d6a
Gustavo Caniati de Lima <50599@labcomp.usc>
2022-11-04 19:27:59 -0300

    beta

--- frontend/src/assets/paginas/components_main/Menu/index.tsx
+++ frontend/src/assets/paginas/components_main/Menu/index.tsx
@@ -6,8 +6,8 @@ function Menu() {
             <a href='http://127.0.0.1:5173/o-que-fazemos' className='imgfund_1 color1'>O Que Fazemos</a>
             <a href='http://127.0.0.1:5173/como-fazemos' className='imgfund_2 color2'>Como Fazemos</a>
             <a href='http://127.0.0.1:5173/crescemos' className='imgfund_3 color3'>Crescemos</a>
-            <a href='http://127.0.0.1:5173/onde-estamos' className='imgfund_4 color1'>Onde Estamos</a>
-            <a href='http://127.0.0.1:5173/dicas' className='imgfund_5 color2'>Dicas</a>
+            <a href='http://127.0.0.1:5173/onde-estamos' className='imgfund_4 color4'>Onde Estamos</a>
+            <a href='http://127.0.0.1:5173/dicas' className='imgfund_5 color5'>Dicas</a>
         </nav>
     )
 }
