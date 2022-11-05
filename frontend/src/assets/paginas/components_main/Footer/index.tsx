cb3279e6286f041320573425da1b17c7b2775ec5
Lucas Barbi de Souza <60655054+LucasBarbi@users.noreply.github.com>
2022-11-04 21:29:46 -0300

    aaa

--- frontend/src/assets/paginas/components_main/Footer/index.tsx
+++ frontend/src/assets/paginas/components_main/Footer/index.tsx
@@ -5,7 +5,7 @@ function Footer(){
         <footer className='footer1'>
             <div>
             <a href='http://127.0.0.1:5173/creditos'> Desenvolvido por </a>
-             <img className='face'/>
+             <img className='face' width="30px" height="30px"/>
             </div>
          
         </footer>
