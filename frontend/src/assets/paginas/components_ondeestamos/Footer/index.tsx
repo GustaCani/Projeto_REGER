833b3c95c119e0e90e8b39e9c73edee2ab94a87b
Gustavo Caniati <caniatigustavo@gmail.com>
2022-11-11 21:13:37 -0300

    site

--- frontend/src/assets/paginas/components_ondeestamos/Footer/index.tsx
+++ frontend/src/assets/paginas/components_ondeestamos/Footer/index.tsx
@@ -3,7 +3,7 @@ import './styles.css'
 function Footer(){
     return(
         <footer className='footer3'>
-            <a href='http://127.0.0.1:5173/creditos'> Desenvolvido por </a>
+            <a href='#/creditos'> Desenvolvido por </a>
         </footer>
     )
 }
