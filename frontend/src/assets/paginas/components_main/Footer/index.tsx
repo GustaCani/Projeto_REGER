88c247417164fc4b8aa49b98b96b576107ee14a8
Lucas Barbi de Souza <60655054+LucasBarbi@users.noreply.github.com>
2022-11-18 18:06:23 -0300

    footer

--- frontend/src/assets/paginas/components_main/Footer/index.tsx
+++ frontend/src/assets/paginas/components_main/Footer/index.tsx
@@ -5,8 +5,15 @@ function Footer(){
         <footer className='footer1'>
             <div>
             <a href='#/creditos'> Desenvolvido por </a>
-             <img className='face' width="30px" height="30px"/>
+             <img className='face' width="30px" height="30px"/>         
             </div>
+        <div>
+        <a href="https://www.facebook.com/UNISAGRADOBauru/" className='text'>Facebook</a>
+        </div>
+
+        <div>
+        <a href="" className='copy'>Copyright © 2022 Unisagrado - Todos os direitos reservados</a>
+        </div>
          
         </footer>
 
