18bbdb6cd09cb44dfe81e4ad247cd2851229ac79
Lucas Barbi de Souza <60655054+LucasBarbi@users.noreply.github.com>
2022-11-23 14:20:43 -0300

    att

--- frontend/src/assets/paginas/components_main/Footer/index.tsx
+++ frontend/src/assets/paginas/components_main/Footer/index.tsx
@@ -5,19 +5,9 @@ function Footer() {
         <footer className='footer1'>
             <div>
                 <a href='#/creditos'> Desenvolvido por </a>
+                <a className='Sobre'> Sobre </a>
                 <img className='face' />
             </div>
-
-            <div className="footer-menu">
-         <ul className="f-menu">
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Contact</a></li>
-            <li><a href="">Blog</a></li>
-        </ul>
-</div>
-
-
         </footer>
 
 
