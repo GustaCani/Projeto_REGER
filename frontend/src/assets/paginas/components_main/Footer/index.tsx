c5e0662d556b714adc41c7a80f26f2b57640259c
Lucas Barbi de Souza <60655054+LucasBarbi@users.noreply.github.com>
2022-11-23 13:53:59 -0300

    footer

--- frontend/src/assets/paginas/components_main/Footer/index.tsx
+++ frontend/src/assets/paginas/components_main/Footer/index.tsx
@@ -6,7 +6,18 @@ function Footer() {
 <div className="footer-content">
 <h3>Foolish Developer</h3>
 </div>
+
+<div className="footer-menu">
+ <ul className="f-menu">
+    <li><a href="">Home</a></li>
+    <li><a href="">About</a></li>
+    <li><a href="">Contact</a></li>
+    <li><a href="">Blog</a></li>
+ </ul>
+</div>
         </footer>
+
+    
 
 
     )
