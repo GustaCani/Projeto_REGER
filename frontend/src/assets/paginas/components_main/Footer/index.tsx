b95c5d94f1892bd891bdc863dd522f6d1c1a6481
Lucas Barbi de Souza <60655054+LucasBarbi@users.noreply.github.com>
2022-11-23 14:12:57 -0300

    aa

--- frontend/src/assets/paginas/components_main/Footer/index.tsx
+++ frontend/src/assets/paginas/components_main/Footer/index.tsx
@@ -8,6 +8,16 @@ function Footer() {
                 <img className='face' width="30px" height="30px" />
             </div>
 
+            <div className="footer-menu">
+         <ul className="f-menu">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Blog</a></li>
+        </ul>
+</div>
+
+
         </footer>
 
 
