3a4ba7c2d508e8ac4f9cfe4adbed1574ef16f66a
Gustavo Caniati <caniatigustavo@gmail.com>
2022-11-22 21:26:26 -0300

    beta

--- frontend/src/assets/paginas/components_main/Footer/index.tsx
+++ frontend/src/assets/paginas/components_main/Footer/index.tsx
@@ -1,13 +1,13 @@
 import './styles.css'
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <footer className='footer1'>
             <div>
-            <a href='#/creditos'> Desenvolvido por </a>
-             <img className='face' width="30px" height="30px"/>
+                <a href='#/creditos'> Desenvolvido por </a>
+                <img className='face' width="30px" height="30px" />
             </div>
-         
+
         </footer>
 
 
