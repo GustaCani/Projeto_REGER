9b32cccddc5c97365a25485a57568f33230eb677
Lucas Barbi de Souza <60655054+LucasBarbi@users.noreply.github.com>
2022-11-23 14:10:06 -0300

    aa

--- frontend/src/assets/paginas/components_main/Footer/index.tsx
+++ frontend/src/assets/paginas/components_main/Footer/index.tsx
@@ -2,24 +2,13 @@ import './styles.css'
 
 function Footer() {
     return (
-        <footer>
-<div className="footer-content">
-<h3>Foolish Developer</h3>
-</div>
+        <footer className='footer1'>
+            <div>
+                <a href='#/creditos'> Desenvolvido por </a>
+                <img className='face' width="30px" height="30px" />
+            </div>
 
-<p>copyright &copy;2021 <a href="#">foolishdeveloper</a>  </p>
-<div className="footer-menu">
-    
- <ul className="f-menu">
-    <li><a href="">Home</a></li>
-    <li><a href="">About</a></li>
-    <li><a href="">Contact</a></li>
-    <li><a href="">Blog</a></li>
- </ul>
-</div>
         </footer>
-
-    
 
 
     )
