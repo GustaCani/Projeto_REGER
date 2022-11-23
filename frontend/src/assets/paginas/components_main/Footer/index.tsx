7be78d370108c5b4d47f1b1a4961450c27a3732f
Lucas Barbi de Souza <60655054+LucasBarbi@users.noreply.github.com>
2022-11-23 13:48:00 -0300

    footer

--- frontend/src/assets/paginas/components_main/Footer/index.tsx
+++ frontend/src/assets/paginas/components_main/Footer/index.tsx
@@ -2,11 +2,7 @@ import './styles.css'
 
 function Footer() {
     return (
-        <footer className='footer1'>
-            <div>
-                <a href='#/creditos'> Desenvolvido por </a>
-                <img className='face' width="30px" height="30px" />
-            </div>
+        <footer>
 
         </footer>
 
