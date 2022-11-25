6812fbd546b6bdc6f7b25b3effabebdebcc335a4
Gustavo Caniati <caniatigustavo@gmail.com>
2022-11-25 13:44:35 -0300

    site

--- frontend/src/assets/paginas/components_oquefazemos/Footer/index.tsx
+++ frontend/src/assets/paginas/components_oquefazemos/Footer/index.tsx
@@ -2,9 +2,8 @@ import './styles.css'
 
 function Footer() {
     return (
-        <footer className='footer1'>
+        <footer className='footer12'>
             <div>
-                <a href='#/creditos'> Desenvolvido por </a>
                 <img className='face' />
             </div>
 
@@ -14,6 +13,11 @@ function Footer() {
                 <a href='#/crescemos' className='crescemos'>Crescemos</a>
                 <a href='#/onde-estamos' className='ondeestamos'>Onde Estamos</a>
                 <a href='#/dicas' className='dicas'>Dicas</a>
+            </div>
+
+            <div>
+                <a href='#/creditos' className='testado'> Desenvolvido por </a>
+                <p className='teste'> © Todos os direitos reservados </p>
             </div>
         </footer>
 
