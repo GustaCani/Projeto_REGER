e12ffeb514c57fd9fe5ad841a116f4dfd5bdc0bd
Lucas Barbi de Souza <60655054+LucasBarbi@users.noreply.github.com>
2022-11-25 09:53:51 -0300

    h

--- frontend/src/assets/paginas/components_main/Header/index.tsx
+++ frontend/src/assets/paginas/components_main/Header/index.tsx
@@ -4,6 +4,7 @@ function Header() {
     return(
         <header className='header1'>
             <h1>PROJETO REGER</h1>
+            <link href="//db.onlinewebfonts.com/c/2fc6d8f828781284dec98fa215c42f8d?family=Leitura+Sans" rel="stylesheet" type="text/css"/>
         </header>
         
     )
